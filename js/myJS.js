f5408d4488cf53033dd2cfc071d786f0604ee289
HieuDo <minhhieu76qng@gmail.com>
2018-10-02 22:56:37 +0700

    Code brand-lable | fix slick arrow | change js slick

--- js/myJS.js
+++ js/myJS.js
@@ -5,8 +5,21 @@ jQuery(function($){
 	// main function
 	VNBuilding.Slider  = function() {
 		$('.slider-bar').slick({
+			autoplay: true,
+			autoplaySpeed: 1500,
 			dots:true,
 			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: true,
+			prevArrow: '<div class="custom-slick-prev"><i class="arrow-prev fa fa-angle-left" aria-hidden="true"></i></div>',
+			nextArrow: '<div class="custom-slick-next"><i class="arrow-next fa fa-angle-right" aria-hidden="true"></i></div>',
+		})
+
+		$('.slider-brand').slick({
+			autoplay: true,
+			autoplaySpeed: 1500,
+			dots:false,
+			slidesToShow: 6,
 			slidesToScroll: 1,
 			arrows: true,
 			prevArrow: '<div class="custom-slick-prev"><i class="arrow-prev fa fa-angle-left" aria-hidden="true"></i></div>',
